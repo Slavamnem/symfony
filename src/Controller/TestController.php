97f0674a21d136c3bfe8d49a0dacc0712fca4df1
Slava <vzelinskiy@stud.onu.edu.ua>
2021-03-26 18:00:09 +0200

    -

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +14,27 @@ class TestController extends AbstractController
      */
     public function index(): Response
     {
-        dd('test!');
-        dd('test!');
+        $response = new Response();
+        $response->headers->setCookie(Cookie::create('test-cookie-2', 25, time() + 300));
+        $response->headers->setCookie(Cookie::create('test-cookie-3', 285, time() + 300));
+
+        phpinfo();
+//        session_start(); session_destroy(); return $response;
+        session_start();
+
+        $count = 0;
+
+        if (isset($_SESSION['count'])) {
+            $count = $_SESSION['count'];
+        }
+
+        $_SESSION['count'] = $count + 1;
+
+        return $response->setContent('Count:' . $_SESSION['count']);
+
+//        dd('test!!');
+//        dd('test!');
+        return $this->render('base.html.twig', ['count' => 5]);
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
